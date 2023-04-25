d502faa1ec5440cd06f5cdcfe5d73f5e0125d6ae
TrungHieu <trunghieunguyendn@gmail.com>
2023-04-25 19:26:25 +0700

    change font

--- lib/commons/constants/theme.dart
+++ lib/commons/constants/theme.dart
@@ -1,5 +1,8 @@
 import 'package:car_rental_locate/commons/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-ThemeData lightTheme =
-    ThemeData(primarySwatch: CustomColors.primaryMaterialColor);
+ThemeData lightTheme = ThemeData(
+  primarySwatch: CustomColors.primaryMaterialColor,
+  fontFamily: GoogleFonts.sourceSansPro().fontFamily,
+);
