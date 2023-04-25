4d10874294a93ba4b97e336db51835e18f7bf901
TrungHieu <trunghieunguyendn@gmail.com>
2023-04-25 19:28:37 +0700

    fix

--- lib/commons/constants/theme.dart
+++ lib/commons/constants/theme.dart
@@ -1,8 +1,16 @@
 import 'package:car_rental_locate/commons/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightTheme = ThemeData(
   primarySwatch: CustomColors.primaryMaterialColor,
+  appBarTheme: const AppBarTheme(
+    color: CustomColors.white,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: CustomColors.white,
+    ),
+  ),
   fontFamily: GoogleFonts.sourceSansPro().fontFamily,
 );
